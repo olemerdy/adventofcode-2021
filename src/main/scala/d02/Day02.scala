b7e147c6f25da7203145136139018e64f48a0b0b
Olivier Le Merdy <olivier.lemerdy+git@gmail.com>
2021-12-02 09:27:39 +0100

    Day2 Part 2

--- src/main/scala/d02/Day02.scala
+++ src/main/scala/d02/Day02.scala
@@ -21,7 +21,27 @@ object Day02 {
       .toList
 }
 
-case class Position(horizontal: Int = 0, depth: Int = 0)
+case class Position(horizontal: Int = 0, depth: Int = 0) {
+  def run(command: Command): Position = command match {
+    case Command(Direction.up, u) =>
+      copy(depth = depth - u)
+    case Command(Direction.down, u) =>
+      copy(depth = depth + u)
+    case Command(Direction.forward, u) =>
+      copy(horizontal = horizontal + u)
+  }
+}
+
+case class PositionWithAim(horizontal: Int = 0, depth: Int = 0, aim: Int = 0) {
+  def run(command: Command): PositionWithAim = command match {
+    case Command(Direction.up, u) =>
+      copy(aim = aim - u)
+    case Command(Direction.down, u) =>
+      copy(aim = aim + u)
+    case Command(Direction.forward, u) =>
+      copy(horizontal = horizontal + u, depth = depth + aim * u)
+  }
+}
 
 enum Direction {
   case forward extends Direction
@@ -32,23 +52,18 @@ enum Direction {
 case class Command(direction: Direction, units: Int)
 
 object Command {
-  @tailrec
-  def run(commands: List[Command], position: Position = Position()): Position =
+  @tailrec def run(commands: List[Command], position: Position): Position =
     if commands.isEmpty then position else
-      val newPosition = commands.head match {
-        case Command(Direction.up, u) =>
-          position.copy(depth = position.depth - u)
-        case Command(Direction.down, u) =>
-          position.copy(depth = position.depth + u)
-        case Command(Direction.forward, u) =>
-          position.copy(horizontal = position.horizontal + u)
-      }
-      run(commands.tail, newPosition)
+      run(commands.tail, position.run(commands.head))
+
+  @tailrec def run(commands: List[Command], position: PositionWithAim): PositionWithAim =
+    if commands.isEmpty then position else
+      run(commands.tail, position.run(commands.head))
 }
 
 object Part1 {
   def result(commands: List[Command]): Int =
-    val position = Command.run(commands)
+    val position = Command.run(commands, Position())
     position.depth * position.horizontal
 
   @main def runPart1(): Unit =
@@ -56,7 +71,9 @@ object Part1 {
 }
 
 object Part2 {
-  def result(lines: List[Command]) = 0
+  def result(commands: List[Command]): Int =
+    val position = Command.run(commands, PositionWithAim())
+    position.depth * position.horizontal
 
   @main def runPart2(): Unit =
     println(result(Day02.myCommands))
--- src/test/scala/d02/Day02Spec.scala
+++ src/test/scala/d02/Day02Spec.scala
@@ -10,14 +10,14 @@ class Day02Spec extends AnyFlatSpec with should.Matchers {
 
 class Part1Spec extends AnyFlatSpec with should.Matchers {
   val expected = 1989265
-  "My input" should "have result of $expected" in {
+  "My input" should s"have result of $expected" in {
     Part1.result(Day02.myCommands) shouldBe expected
   }
 }
 
 class Part2Spec extends AnyFlatSpec with should.Matchers {
-  val expected = 0
-  "My input" should "have result of $expected" in {
+  val expected = 2089174012
+  "My input" should s"have result of $expected" in {
     Part2.result(Day02.myCommands) shouldBe expected
   }
 }
