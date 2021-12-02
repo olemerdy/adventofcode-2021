42b19226728a0abd0eddafabd949d59182b82c79
Olivier Le Merdy <olivier.lemerdy+git@gmail.com>
2021-12-02 09:37:12 +0100

    Day2 Part 2

--- src/test/scala/d02/Day02Spec.scala
+++ src/test/scala/d02/Day02Spec.scala
@@ -4,11 +4,20 @@ package d02
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.*
 
-class Day02Spec extends AnyFlatSpec with should.Matchers {
-
-}
+val sample: List[Command] =
+  Command(Direction.forward, 5) ::
+    Command(Direction.down, 5) ::
+    Command(Direction.forward, 8) ::
+    Command(Direction.up, 3) ::
+    Command(Direction.down, 8) ::
+    Command(Direction.forward, 2) ::
+    Nil
 
 class Part1Spec extends AnyFlatSpec with should.Matchers {
+  "Sample input" should "have result of 150" in {
+    Part1.result(sample) shouldBe 150
+  }
+
   val expected = 1989265
   "My input" should s"have result of $expected" in {
     Part1.result(Day02.myCommands) shouldBe expected
@@ -16,6 +25,10 @@ class Part1Spec extends AnyFlatSpec with should.Matchers {
 }
 
 class Part2Spec extends AnyFlatSpec with should.Matchers {
+  "Sample input" should "have result of 900" in {
+    Part2.result(sample) shouldBe 900
+  }
+
   val expected = 2089174012
   "My input" should s"have result of $expected" in {
     Part2.result(Day02.myCommands) shouldBe expected
